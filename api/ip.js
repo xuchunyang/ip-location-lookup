360d010d6ec146bd0158dd1b218aee28e292dd18
Xu Chunyang <xuchunyang56@gmail.com>
2020-08-08 21:32:55 +0800

    Fix ip address
    https://stackoverflow.com/a/8107922/2999892

--- api/ip.js
+++ api/ip.js
@@ -1,5 +1,10 @@
 module.exports = (req, res) => {
   res.statusCode = 200;
-  const ip = req.connection.remoteAddress;
-  res.end(ip);
+  // const ip = req.connection.remoteAddress;
+  const ip = req.headers["x-forwarded-for"]
+  res.end(JSON.stringify(
+    {ip, headers: req.headers},
+    null,
+    4
+  ));
 };
